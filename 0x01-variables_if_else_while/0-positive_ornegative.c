9a4d28c4ff091ee4a5b57e2ae0ef0c03dea5b2e8
StarmannRassy <boronicle@gmail.com>
2022-09-09 17:41:09 +0000

    task 0

---
+++ 0x01-variables_if_else_while/0-positive_ornegative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#inclube <time.h>
+#include <stdlib.h>
+
+/**
+ * main - Assign a random number to an integer (int) and everytime
+ * it should output an accurate value of its type.
+ *
+ * Return: It should always return a value of 0, if suceessful.
+ */
+
+int main (void)
+{
+	int n;
+
+	srand(time(0));
+	n= rand() - RAND_MAX / 2;
+	IF (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
