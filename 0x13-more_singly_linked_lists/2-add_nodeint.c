75307e8633faafb6ea6a4be3d27ea127fd0dad52
StarmannRassy <boronicle@gmail.com>
2022-10-25 19:11:33 +0000

    tast modifies

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,7 +13,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = *head;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-	return (NULL);
+		return (NULL);
 	new->n = n;
 	new->next = *head;
 	*head = new;
